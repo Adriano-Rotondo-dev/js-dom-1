e29de3b073cddb1f96750fc6645638f6b9ba8608
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-05 15:54:41 +0200

    add imgEl node, add event imgEl switch to lightSwitchBtnEl to swap lightBulb image to yellow_lamp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,19 @@
 //TODO: al click del bottone "lightSwitch" la lampadina dovrà "accendersi"
 //* visualizzo in console il click del bottone
 //prima seleziono il bottone
-const lightSwitchBtnEl = document.getElementById('lightSwitch') // salva il DOM node
+const lightSwitchBtnEl = document.getElementById("lightSwitch"); // salvo il DOM node
 
-// applico l'event listener al bottone 
-lightSwitchBtnEl.addEventListener('click', function () {
-    // stampo in console il click
-    console.log("Hai cliccato")
-})
-
+// seleziono l'immagine
+const imgEl = document.getElementById("lightBulb"); // salvo il DOM node
 
 //* collego al bottone il cambio di immagine della lampadina
-
+// applico l'event listener al bottone
+lightSwitchBtnEl.addEventListener("click", function () {
+  // stampo in console il click
+  console.log("Hai cliccato");
+  // una volta constatato il suo funzionamento, aggiungo l'event per cambiare l'immagine
+  imgEl.src = "./assets/img/yellow_lamp.png";
+});
 
 //! BONUS
 //TODO: il pulsante, una volta premuto, cambierà in "spegni", riportando l'immagine originale
