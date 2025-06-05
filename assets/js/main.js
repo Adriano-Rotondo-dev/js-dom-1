0cde079453f4707c54332e1831245875ba8a5cf8
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-05 15:32:46 +0200

    add container to center contents, wrote human code to organize workflow

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+//TODO: al click del bottone "lightSwitch" la lampadina dovrà "accendersi"
+//* visualizzo in console il click del bottone
+//* collego al bottone il cambio di immagine della lampadina
+
+
+//! BONUS
+//TODO: il pulsante, una volta premuto, cambierà in "spegni", riportando l'immagine originale
+//* deve funzionare in loop
+//* string.includes() potrebbe essere necessario
+//? cambio anche il colore del body con l'accensione?
