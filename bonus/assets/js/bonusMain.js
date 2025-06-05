363f0339fc75f7f24bcce3a902fdafe420eeb2e0
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-05 16:29:00 +0200

    add bonus folder, add lightSwitch event to light up and shut down lightBulb image and button

---
+++ bonus/assets/js/bonusMain.js
@@ -0,0 +1,34 @@
+//TODO: al click del bottone "lightSwitch" la lampadina dovrà "accendersi"
+//! BONUS
+//TODO: il pulsante, una volta premuto, cambierà in "spegni", riportando l'immagine originale
+//* deve funzionare in loop
+//* string.includes() potrebbe essere necessario
+//? cambio anche il colore del body con l'accensione?
+
+
+//* visualizzo in console il click del bottone
+//prima seleziono il bottone
+const lightSwitchBtnEl = document.getElementById("lightSwitch"); // salvo il DOM node
+
+// seleziono l'immagine
+const imgEl = document.getElementById("lightBulb"); // salvo il DOM node
+
+// seleziono il body per il cambio di background color
+const bgEl = document.getElementById("pageBody")
+
+//* collego al bottone il cambio di immagine della lampadina
+// applico l'event listener al bottone
+lightSwitchBtnEl.addEventListener("click", function () {
+  // stampo in console il click
+  console.log("Hai cliccato");
+  // una volta constatato il suo funzionamento, aggiungo l'event per cambiare l'immagine
+  if (imgEl.src.includes("white_lamp.png")) {
+    imgEl.src = "./assets/img/yellow_lamp.png";
+    lightSwitchBtnEl.textContent = "Spegni";
+  } else if (imgEl.src.includes("yellow_lamp.png")) {
+    imgEl.src = "./assets/img/white_lamp.png";
+    lightSwitchBtnEl.textContent = "Accendi";
+  }
+
+});
+
