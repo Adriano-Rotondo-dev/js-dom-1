b68c0ea2c0a9e6af33a2696c8a73866372598bc9
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-05 16:39:16 +0200

    refactor bonusMain.js, removed unnecessary element, add comments for a clearer workflow

--- bonus/assets/js/bonusMain.js
+++ bonus/assets/js/bonusMain.js
@@ -4,27 +4,28 @@
 //* deve funzionare in loop [un toggle?]
 //* string.includes() potrebbe essere necessario [LO È]
 
-//* visualizzo in console il click del bottone
 //prima seleziono il bottone
 const lightSwitchBtnEl = document.getElementById("lightSwitch"); // salvo il DOM node
 
 // seleziono l'immagine
 const imgEl = document.getElementById("lightBulb"); // salvo il DOM node
 
-// seleziono il body per il cambio di background color
-const bgEl = document.getElementById("pageBody");
-
 //* collego al bottone il cambio di immagine della lampadina
 // applico l'event listener al bottone
 lightSwitchBtnEl.addEventListener("click", function () {
+  //* visualizzo in console il click del bottone
   // stampo in console il click
   console.log("Hai cliccato");
   // una volta constatato il suo funzionamento, aggiungo l'event per cambiare l'immagine
+  // imposto le condizioni per cambiare l'immagine da white_lamp a yellow_lamp
   if (imgEl.src.includes("white_lamp.png")) {
     imgEl.src = "./assets/img/yellow_lamp.png";
+    // imposto la condizione per cambiare il testo del bottone
     lightSwitchBtnEl.textContent = "Spegni";
+    // imposto la condizioni per cambiare l'immagine da yellow_lamp a white_lamp
   } else if (imgEl.src.includes("yellow_lamp.png")) {
     imgEl.src = "./assets/img/white_lamp.png";
+    // imposto la condizione per cambiare il testo del bottone
     lightSwitchBtnEl.textContent = "Accendi";
   }
 });
