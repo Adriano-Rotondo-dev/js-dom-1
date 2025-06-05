8f918bd5f14daf64c3c16015f01e9b3a73a7c70b
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-05 15:47:40 +0200

    add DOM node lightSwitchBtnEl, add event listener to lightSwitch button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 //TODO: al click del bottone "lightSwitch" la lampadina dovrà "accendersi"
 //* visualizzo in console il click del bottone
+//prima seleziono il bottone
+const lightSwitchBtnEl = document.getElementById('lightSwitch') // salva il DOM node
+
+// applico l'event listener al bottone 
+lightSwitchBtnEl.addEventListener('click', function () {
+    // stampo in console il click
+    console.log("Hai cliccato")
+})
+
+
 //* collego al bottone il cambio di immagine della lampadina
 
 
